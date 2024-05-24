6c6d4a4cdc3cf3a0c0be32cb642cd3cf0403112a
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-24 08:17:11 -0700

    Layout component - Renders the main layout of the application, conditionally displaying either the full page layout for authenticated users or redirecting unauthenticated users to the login page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,46 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-no-undef */
-import {Routes,Route,Navigate,Outlet,useLocation} from "react-router-dom"; 
-import Login from './pages/Login'
-import Dashboard from "./pages/Dashboard";
+import { Transition } from "@headlessui/react";
+import clsx from "clsx";
+import { Fragment, useRef } from "react";
+import { IoClose } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Login from "./pages/Login";
+import TaskDetails from "./pages/TaskDetails";
+import Tasks from "./pages/Tasks";
+import Trash from "./pages/Trash";
+import Users from "./pages/Users";
+import Dashboard from "./pages/Dashboard";
+import { setOpenSidebar } from "./redux/slices/authSlice";
 function App() {
   
-
+  function Layout() {
+    const { user } = useSelector((state) => state.auth);
+  
+    const location = useLocation();
+    return user ? (
+      <div className='w-full h-screen flex flex-col md:flex-row'>
+        <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+          {/* <Sidebar /> */}
+        </div>
+  
+        {/* <MobileSidebar /> */}
+  
+        <div className='flex-1 overflow-y-auto'>
+          <Navbar />
+  
+          <div className='p-4 2xl:px-10'>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    ) : (
+      <Navigate to='/log-in' state={{ from: location }} replace />
+    );
+  }
   return (
     <main className='w-full min-h-screen bg-[#f3f4f6] '>
       <Routes>
