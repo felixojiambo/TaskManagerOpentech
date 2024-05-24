cef5782c66a9b3407483cf2492c545cb57899869
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-24 08:56:03 -0700

    feat(AddTask): Implement modal for adding/updating tasks with detailed form

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 
-const Task = () => {
+const Tasks = () => {
   return (
     <div>Task</div>
   )
 }
 
-export default Task
+export default Tasks
