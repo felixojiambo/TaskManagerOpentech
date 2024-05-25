174813e7fe2d0960db6cedf587865a8c3249a8cc
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-25 05:13:07 -0700

    feat: Implement Layout and MobileSidebar components for responsive navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,34 +13,87 @@ import TaskDetails from "./pages/TaskDetails";
 import Tasks from "./pages/Tasks";
 import Trash from "./pages/Trash";
 import Users from "./pages/Users";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
 import { setOpenSidebar } from "./redux/slices/authSlice";
-function App() {
-  
-  function Layout() {
-    const { user } = useSelector((state) => state.auth);
-  
-    const location = useLocation();
-    return user ? (
-      <div className='w-full h-screen flex flex-col md:flex-row'>
-        <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
-          {/* <Sidebar /> */}
-        </div>
-  
-        {/* <MobileSidebar /> */}
-  
-        <div className='flex-1 overflow-y-auto'>
-          <Navbar />
-  
-          <div className='p-4 2xl:px-10'>
-            <Outlet />
-          </div>
+
+function Layout() {
+  const { user } = useSelector((state) => state.auth);
+
+  const location = useLocation();
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+        <Sidebar />
+      </div>
+
+      <MobileSidebar />
+
+      <div className='flex-1 overflow-y-auto'>
+        <Navbar />
+
+        <div className='p-4 2xl:px-10'>
+          <Outlet />
         </div>
       </div>
-    ) : (
-      <Navigate to='/login' state={{ from: location }} replace />
-    );
-  }
+    </div>
+  ) : (
+    <Navigate to='/login' state={{ from: location }} replace />
+  );
+}
+
+const MobileSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.auth);
+  const mobileMenuRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false));
+  };
+
+  return (
+    <>
+      <Transition
+        show={isSidebarOpen}
+        as={Fragment}
+        enter='transition-opacity duration-700'
+        enterFrom='opacity-x-10'
+        enterTo='opacity-x-100'
+        leave='transition-opacity duration-700'
+        leaveFrom='opacity-x-100'
+        leaveTo='opacity-x-0'
+      >
+        {(ref) => (
+          <div
+            ref={(node) => (mobileMenuRef.current = node)}
+            className={clsx(
+              "md:hidden w-full h-full bg-black/40 transition-all duration-700 transform ",
+              isSidebarOpen ? "translate-x-0" : "translate-x-full"
+            )}
+            onClick={() => closeSidebar()}
+          >
+            <div className='bg-white w-3/4 h-full'>
+              <div className='w-full flex justify-end px-5 mt-5'>
+                <button
+                  onClick={() => closeSidebar()}
+                  className='flex justify-end items-end'
+                >
+                  <IoClose size={25} />
+                </button>
+              </div>
+
+              <div className='-mt-10'>
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </>
+  );
+};
+
+function App() {
   return (
     <main className='w-full min-h-screen bg-[#f3f4f6] '>
       <Routes>
@@ -64,4 +117,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
