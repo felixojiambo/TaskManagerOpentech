3ce5a2304bafe4eefcb0e417fa848c5994063255
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-24 22:51:06 -0700

    Introduce NotificationPanel component with support for displaying alerts and messages. Utilizes Headless UI for accessibility and styling, Moment.js for date formatting, and React Icons for visual representation of notification types.

---
+++ client/src/assets/data.js
@@ -0,0 +1,44 @@
+const tasksData = [
+    {
+      "_id": "65c5f12ab5204a81bde866a9",
+      "title": "Website Redesign",
+      "date": "2024-02-15T00:00:00.000Z",
+      "priority": "high",
+      "stage": "in progress",
+      "description": "Redesign the website for better performance.",
+      "category": "Web Development",
+      "email": "webdev@example.com",
+      "phone": "555-123-4567",
+      "createdAt": "2024-02-15T00:00:00.000Z",
+      "updatedAt": "2024-02-15T00:00:00.000Z"
+    },
+    {
+      "_id": "65c202d4aa62f32ffd1303cc",
+      "title": "New Feature Implementation",
+      "date": "2024-02-20T00:00:00.000Z",
+      "priority": "medium",
+      "stage": "todo",
+      "description": "Implement a new feature for improved functionality.",
+      "category": "Product Management",
+      "email": "newfeature@example.com",
+      "phone": "555-987-6543",
+      "createdAt": "2024-02-20T00:00:00.000Z",
+      "updatedAt": "2024-02-20T00:00:00.000Z"
+    },
+    {
+      "_id": "65c30b96e639681a13def0b5",
+      "title": "Complete User Testing",
+      "date": "2024-02-22T00:00:00.000Z",
+      "priority": "low",
+      "stage": "in progress",
+      "description": "Conduct thorough user testing to identify usability issues.",
+      "category": "Quality Assurance",
+      "email": "qa@example.com",
+      "phone": "555-555-5555",
+      "createdAt": "2024-02-22T00:00:00.000Z",
+      "updatedAt": "2024-02-22T00:00:00.000Z"
+    }
+  ];
+  
+  export default tasksData;
+  
