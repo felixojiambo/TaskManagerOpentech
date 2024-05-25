b657515008394cda673f6e615b73aee4da225f86
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-25 09:46:38 -0700

    feat: Set up Express server with CORS, middleware, and error handling

--- server/index.js
+++ server/index.js
@@ -13,3 +13,23 @@ dbConnection();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "http://localhost:3001"],
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      credentials: true,
+    })
+  );
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  
+  app.use(cookieParser());
+  
+  app.use(morgan("dev"));
+  app.use("/api", routes);
+  
+  app.use(routeNotFound);
+  app.use(errorHandler);
+  
+  app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
